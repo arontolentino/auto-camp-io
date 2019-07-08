3be15189e7aea113e97334ad9d9a55f955533fbb
Aron Tolentino <arontolentino@outlook.com>
2019-07-08 19:16:24 -0400

    Add car overview routes
    - Get All Cars
    - Post Car

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+// HTTP request logger middleware
+const morgan = require('morgan');
+
+const carRouter = require('./routes/carRoutes');
+
+const app = express();
+
+// ============================ //
+// MIDDLEWARES
+// ============================ //
+
+// Only run HTTP request logger during dev mode
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Used to show time on an HTTP request
+app.use((req, rest, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// Parses incoming requests with JSON payloads and is based on body-parser
+app.use(express.json());
+
+// Used to serve static files
+app.use(express.static(`${__dirname}/public`));
+
+// ============================ //
+// ROUTES
+// ============================ //
+
+app.use('/api/v1/cars', carRouter);
+
+module.exports = app;
