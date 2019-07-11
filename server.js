ba5df5c24226badc8e2091f72b9626bb1870f4f7
Aron Tolentino <arontolentino@outlook.com>
2019-07-10 22:31:54 -0400

    Create simple car mongoose model

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
+// Connected to mongoose database
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -20,6 +21,25 @@ mongoose
   .then(() => {
     console.log('DB connection successful');
   });
+
+// Creating mongoose schema
+const carSchema = new mongoose.Schema({
+  make: {
+    type: String,
+    required: [true, 'A car must have a make']
+  },
+  model: {
+    type: String,
+    required: [true, 'A car must have a model'],
+    default: 'Unknown'
+  },
+  year: {
+    type: Number,
+    required: [true, 'A car must have a year manufactured']
+  }
+});
+
+const Car = mongoose.model('Car', carSchema);
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
