5343d4c4c711b83c0ffab34a21439a4d8e50c7f2
Aron Tolentino <arontolentino@outlook.com>
2019-07-10 22:15:58 -0400

    Connect MongoDB Atlas to express app

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
+const mongoose = require('mongoose');
 // Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env
 const dotenv = require('dotenv');
+const app = require('./app');
 
+// Reading enivonrment variables
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('DB connection successful');
+  });
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
