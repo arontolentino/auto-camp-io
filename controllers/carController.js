a6e1abe3433f448469bf15cd86e2be1070649e23
Aron Tolentino <arontolentino@outlook.com>
2019-07-11 22:19:06 -0400

    Refactor getAllCars and getCar function to read from mongodb

--- controllers/carController.js
+++ controllers/carController.js
@@ -1,23 +1,41 @@
 //const fs = require('fs');
 const Car = require('./../models/carModels');
 
-exports.getAllCars = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime
-    // results: cars.length,
-    // data: {
-    //   cars
-    // }
-  });
+exports.getAllCars = async (req, res) => {
+  try {
+    const cars = await Car.find();
+
+    res.status(200).json({
+      status: 'success',
+      results: cars.length,
+      data: {
+        cars
+      }
+    });
+  } catch (err) {
+    res.status(404).jason({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
-exports.getCar = (req, res) => {
-  console.log(req.params);
+exports.getCar = async (req, res) => {
+  try {
+    const car = await Car.findById(req.params.id);
 
-  // Converts string from req.params to number
-  const id = req.params.id * 1;
+    res.status(200).json({
+      status: 'success',
+      data: {
+        car
+      }
+    });
+  } catch (err) {
+    res.status(404).jason({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 exports.createCar = async (req, res) => {
