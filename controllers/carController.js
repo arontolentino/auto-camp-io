e7106a67baf39fc590730cfd13c8b8e8b5620c3b
Aron Tolentino <arontolentino@outlook.com>
2019-07-11 21:49:44 -0400

    Refactor createCar function to create new mongodb entry
    Switched from adding entry to local json file to mongodb car collection

--- controllers/carController.js
+++ controllers/carController.js
@@ -1,45 +1,15 @@
-const fs = require('fs');
-
-// JSON.parse parses json file into an array of JavaScript object
-const cars = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/cars.json`)
-);
-
-// Only use val when using router.param
-exports.checkID = (req, res, next, val) => {
-  console.log(`Car id is: ${val}`);
-
-  if (req.params.id * 1 > cars.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-  // Runs the next function
-  next();
-};
-
-exports.checkBody = (req, res, next) => {
-  console.log(`Car name is: ${req.body.name}`);
-
-  if (!req.body.name) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name'
-    });
-  }
-  next();
-};
+//const fs = require('fs');
+const Car = require('./../models/carModels');
 
 exports.getAllCars = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
-    requestedAt: req.requestTime,
-    results: cars.length,
-    data: {
-      cars
-    }
+    requestedAt: req.requestTime
+    // results: cars.length,
+    // data: {
+    //   cars
+    // }
   });
 };
 
@@ -48,41 +18,27 @@ exports.getCar = (req, res) => {
 
   // Converts string from req.params to number
   const id = req.params.id * 1;
-
-  // find() method returns the value of the first element in the array that satisfies the provided testing function
-  const car = cars.find(el => el.id === id);
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      car
-    }
-  });
 };
 
-exports.createCar = (req, res) => {
-  // console.log(req.body);
-  const newId = cars[cars.length - 1].id + 1;
-  // Object.assign creates a new object by merging two existing objects together
-  const newCar = Object.assign({ id: newId }, req.body);
+exports.createCar = async (req, res) => {
+  try {
+    // const newTour = new Tour({})
+    // newTour.save()
 
-  // Pushes newCar to array
-  cars.push(newCar);
+    const newCar = await Car.create(req.body);
 
-  // Overwrites cars.js with new array
-  fs.writeFile(
-    `${__dirname}/dev-data/data/cars.json`,
-    // JSON.stringfy() method converts a JavaScript onbject or value to a JSON string
-    JSON.stringify(cars),
-    err => {
-      res.status(201).json({
-        status: 'sucess',
-        data: {
-          car: newCar
-        }
-      });
-    }
-  );
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newCar
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 exports.updateCar = (req, res) => {
--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -4,12 +4,12 @@ const carController = require('./../controllers/carController');
 const router = express.Router();
 
 // Checks if id exists using checkID middleware
-router.param('id', carController.checkID);
+// router.param('id', carController.checkID);
 
 router
   .route('/')
   .get(carController.getAllCars)
-  .post(carController.checkBody, carController.createCar);
+  .post(carController.createCar);
 
 router
   .route('/:id')
