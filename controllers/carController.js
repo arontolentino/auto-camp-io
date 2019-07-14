f7013c2ec68e33d11dddbe26180e8712684a0730
Aron Tolentino <arontolentino@outlook.com>
2019-07-14 12:29:41 -0400

    Implement limiting fields for getAllCars function

--- controllers/carController.js
+++ controllers/carController.js
@@ -25,6 +25,14 @@ exports.getAllCars = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     // EXECUTE QUERY
     const cars = await query;
 
