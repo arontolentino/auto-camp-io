d76ff828d9d0d7e24ec67304b4b5be9925c80aa5
Aron Tolentino <arontolentino@outlook.com>
2019-07-14 12:55:07 -0400

    Implement pagination for getAllCars function

--- controllers/carController.js
+++ controllers/carController.js
@@ -33,9 +33,22 @@ exports.getAllCars = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Car.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exists');
+    }
+
     // EXECUTE QUERY
     const cars = await query;
 
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: cars.length,
