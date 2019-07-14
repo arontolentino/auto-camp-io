cb2a87610f73c9db5930d9aaa647be0b117eed62
Aron Tolentino <arontolentino@outlook.com>
2019-07-14 12:16:27 -0400

    Implement sorting for getAllCars function
    Default sorting based on when car entry was created

--- controllers/carController.js
+++ controllers/carController.js
@@ -4,18 +4,26 @@ const Car = require('./../models/carModels');
 exports.getAllCars = async (req, res) => {
   try {
     // BUILD QUERY
-    // 1) Filtering
+    // 1A) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
 
-    // 2) Advanced Filtering
+    // 1B) Advanced Filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
     console.log(JSON.parse(queryStr));
 
-    const query = await Car.find(JSON.parse(queryStr));
+    let query = Car.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // EXECUTE QUERY
     const cars = await query;
